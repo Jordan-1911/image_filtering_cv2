34d210364b75925fe918af8b65603a6099a1203c
jordan-1911 <jordanwashburn95@gmail.com>
2023-04-09 16:48:15 -0500

    Gaussian Blur added

--- smoothing_images_and_blurring.py
+++ smoothing_images_and_blurring.py
@@ -17,17 +17,17 @@ kernel = np.ones((5, 5), np.float32) / 25  # kernel is defines by 1 / (K_width *
 # arguments = image, desired depth of the destination image,  3rd is the kernel
 dst = cv2.filter2D(img, -1, kernel)
 
-titles = ['image', '2D Convolution']
-images = [img, dst]
+# titles = ['image', '2D Convolution']
+# images = [img, dst]
 
-for i in range(2):
-    plt.subplot(1, 2, i + 1)
-    plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([])
-    plt.yticks([])
+# for i in range(2):
+#     plt.subplot(1, 2, i + 1)
+#     plt.imshow(images[i], 'gray')
+#     plt.title(titles[i])
+#     plt.xticks([])
+#     plt.yticks([])
     
-plt.show()
+# plt.show()
 
 # the result should show the first image unfiltered, with the second image with less noise and a bit more blur
 
@@ -36,4 +36,36 @@ plt.show()
 # HPF - helps in finding edges in the images
 
 
+# used to blur images, takes in image, and kernel
+# blur = cv2.blur(img, (5, 5))
+# titles = ['images', '2D Convolution', 'blur']
+# images = [img, dst, blur]
 
+# for i in range(3):
+#     plt.subplot(1, 3, i + 1)
+#     plt.imshow(images[i], 'gray')
+#     plt.title(titles[i])
+#     plt.xticks()
+#     plt.yticks()
+    
+# plt.show()
+
+# GAUSSIAN FILTER
+# Guassian filter is just using different kernel in both x and y direction
+# kernel matrix has highest value in the middle and decreases as we traverse outward
+
+blur = cv2.blur(img, (5, 5))
+g_blur = cv2.GaussianBlur(img, (5, 5), 0)
+titles = ['images', '2D Convolution', 'blur', 'Gaussian Blur']
+images = [img, dst, blur,g_blur]
+
+for i in range(4):
+    plt.subplot(1, 4, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks()
+    plt.yticks()
+
+plt.show()
+    
+    
